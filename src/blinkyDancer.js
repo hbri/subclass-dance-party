3dbcaf7edb9c94b1d0959c251e3ede1d581d0fb8
Brian Hauck <brihauck4@gmail.com>
2020-08-24 17:27:22 -0700

    update to pseudoclassical

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,23 +1,25 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-  // makeDancer.call(this, top, left, timeBetweenSteps);
+  // var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
-  var oldStep = blinkyDancer.step;
+  var oldStep = makeBlinkyDancer.prototype.step;
 
-  blinkyDancer.step = function() {
+
+  this.step = function() {
     // call the old version of step at the beginning of any call to this new version of step
     oldStep();
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-    blinkyDancer.$node2.toggle();
+    this.$node.toggle();
+    this.$node2.toggle();
   };
 
-  return blinkyDancer;
+  // return blinkyDancer;
 };
 
 // superClass - basic properties of a dancer
--- src/init.js
+++ src/init.js
@@ -22,19 +22,19 @@ $(document).ready(function() {
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
-    $('body').append(dancer.$node);
+    $('body').append(makeBlinkyDancer.prototype.$node);
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
-    $('body').append(dancer.$node2);
+    $('body').append(makeBlinkyDancer.prototype.$node2);
   });
 });
 
